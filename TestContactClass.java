132a74b8dc15606dbd45b230869ec11cb01b2904
AmeenPopoola <82396792+AmeenPopoola@users.noreply.github.com>
2021-04-12 16:29:10 +0100

    Add files via upload

---
+++ TestContactClass.java
@@ -0,0 +1,41 @@
+public class TestContactClass {
+    public static void main(String[] args) {
+        
+        Contact gon = new Contact("Gerry O'Neill","001 123 7654321","GON@genius.ie",
+                                     "@CSGenius","@Prolog","@NotOnTwitter");
+        Contact dm = new Contact("Diego Maradona","+771 0896 651299452","TheHandOfGod@1986.WorldCup",
+                                     "@TheAllTimeGreatestArgentinianFootballLegend","","@Maradona");
+        Contact jb = new Contact("Josephine Bloggs","086 45216789","","","","@Bloggology");
+        
+        System.out.println(gon);
+        System.out.println(dm);
+        System.out.println(jb);
+        
+        Contact ewd = new Contact("Edsger Dijkstra","151 698 213459","EWD@uniTexas.com",
+                                     "","","");
+        Contact dk = new Contact("Donald Knuth","001 241 721890","knuth@stanford.com",
+                                     "","","");
+        Contact nw = new Contact("Nik Wirth","080 810 2468312","nik@zurichETH.swiss",
+                                     "","","");
+
+        System.out.println(ewd);
+        System.out.println(dk);
+        System.out.println(nw);
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ 
